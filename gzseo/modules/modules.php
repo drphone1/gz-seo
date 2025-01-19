e3f5cd372df48a293405af46166f128551192676
mohammadshahabi <75863898+drphone1@users.noreply.github.com>
2025-01-19 13:42:01 +0330

    Delete gzseo directory

--- gzseo/modules/modules.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('ABSPATH')) exit;
-require_once(GZSEO_PLUGIN_DIR.'modules/gzseo.php');
-require_once(GZSEO_PLUGIN_DIR.'modules/gsc.php');
-require_once(GZSEO_PLUGIN_DIR.'modules/comment.php');
-require_once(GZSEO_PLUGIN_DIR.'modules/ai.php');
